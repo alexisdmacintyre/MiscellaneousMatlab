ad26a4e2b5d75df24f00a520c95c779681bdeb2c
Alexis Deighton MacIntyre <alexisdharrison@gmail.com>
2020-10-12 18:30:42 +0100

    Update calculateAsynchrony.m
    Added optional arguments out for specifying which data points were paired and which weren't

--- calculateAsynchrony.m
+++ calculateAsynchrony.m
@@ -1,4 +1,4 @@
-function [asynchronies,unmatched1,unmatched2] = calculateAsynchrony(vector1,vector2,plotMatches)
+function [asynchronies,unmatched1,unmatched2,matched1,matched2] = calculateAsynchrony(vector1,vector2,plotMatches)
 
 % Calculates asynchrony (absolute distance) between paired datapoints from
 % two vectors. This function was developed for rhythm behavioural 
@@ -6,225 +6,281 @@ function [asynchronies,unmatched1,unmatched2] = calculateAsynchrony(vector1,vect
 
 % For each observation, the closest two-way matched timepoints
 % are found and their absolute asynchrony is calculated using euclidean
-% distance. Single unpaired points are ignored, i.e., there is no 'penalty'
-% or note for unpaired points, so adjust accordingly.
+% distance. Remaining unpaired points are ignored, and can be specified
+% by using optional output arguments that return their indices.
 
 % Optional 2nd and 3rd outputs return a vector containing unpaired indices
-% from vectors 1 and 2, respectively.
+% from vectors 1 and 2, respectively; 4th and 5th outputs are the paired
+% equivalents.
 
 % The method attempts to account for ordinality, in case a series of
 % best pairings are consistently a little out of time (but not out of step)
 % from each other.
 
 % To visualise the pairings and make sure it's working right, use the 
-% optional 3rd input argument (set to 1).
+% optional 3rd input argument (set flag to 1).
 
 % Example usage including a visualisation:
 % asynchronies = calculateAsynchrony(participant1Drum,participant2Drum,1)
 
-% Example usage without visualisation:
-% [asynchronies,unmatched1,unmatched2] = calculateAsynchrony(participant1Drum,participant2Drum)
+% Example usage without visualisation, only returning paired and unpaired
+% datapoints from participant 1:
+% [asynchronies,unmatched1,~,matched1,~] = calculateAsynchrony(participant1Drum,participant2Drum)
 
+% comments/suggations: alexisdharrison@gmail.com or a.macintyre.17@ucl.ac.uk
 
-% comments/suggations: alexisdharrison@gmail.com
+vector1 = sort(vector1);
+vector2 = sort(vector2);
 
 if ~exist('plotMatches','var') || plotMatches~=1
     plotMatches = 0;
 end
 
-if numel(vector1) > numel(vector2) % Longer vector becomes vectorB
-    vectorA = vector2;
-    vectorB = vector1;
-    longerVector = 1;
-elseif numel(vector1) < numel(vector2)
-    vectorB = vector2;
-    vectorA = vector1;    
-    longerVector = 2;
-else
-    vectorA = vector1;
-    vectorB = vector2;
-    longerVector = 0;
-end
-
-if plotMatches == 1 % plot both vectors together
-    close all
-    scatter(vectorA,repmat(1,numel(vectorA),1))
-    hold on
-    scatter(vectorB,repmat(1.1,numel(vectorB),1))
-    ylim([.95 1.15])
-end
-
-% Pair up observations
-counter = 1;
-asynchronies = [];
-numObs = numel(vectorB);
-
-for i = 1:numObs
-    if sum(~isinf(vectorA))~=0 && sum(~isinf(vectorB))~=0 % stop when one vector runs out
+if numel(vector1)>0 && numel(vector2)>0
+    
+    if size(vector1,1) == 1
+        vector1 = vector1';
+    end
+    
+    if size(vector2,1) == 1
+        vector2 = vector2';
+    end
+    
+    if numel(vector1) > numel(vector2) % Longer vector becomes vectorB
+        vectorA = vector2;
+        vectorB = vector1;
         
-        maxLen = max(numel(vectorB),numel(vectorA));
-        padding = inf(maxLen,1);
-        vectorA = [vectorA ; padding(1:abs(numel(vectorA)-maxLen))];
-        vectorB = [vectorB ; padding(1:abs(numel(vectorB)-maxLen))];
+        longerVector = 1;
         
-        [closestA,idxA] = min(abs(vectorA(i)-vectorB));
-        [closestB,idxB] = min(abs(vectorB(i)-vectorA));
-        [~,idx] = max([idxA idxB]);
+    elseif numel(vector1) < numel(vector2)
+        vectorB = vector2;
+        vectorA = vector1;
         
-        if idxA == idxB % vectorA(i)/vectorB(i) are each other's closest match
+        longerVector = 2;
+        
+    else
+        vectorA = vector1;
+        vectorB = vector2;
+        
+        longerVector = 0;
+    end
+    
+    if plotMatches == 1 % plot both vectors together
+        close all
+        scatter(vectorA,repmat(1,numel(vectorA),1))
+        hold on
+        scatter(vectorB,repmat(1.1,numel(vectorB),1))
+        ylim([.95 1.15])
+    end
+    
+    % Pair up observations to determine asynchronies between speakers
+    counter = 1;
+    asynchronies = [];
+    numObs = numel(vectorB);
+    
+    for i = 1:numObs
+        if sum(~isinf(vectorA))~=0 && sum(~isinf(vectorB))~=0
             
-            asynchronies(counter) = closestA;
+            maxLen = max(numel(vectorB),numel(vectorA));
+            padding = inf(maxLen,1);
+            vectorA = [vectorA ; padding(1:abs(numel(vectorA)-maxLen))];
+            vectorB = [vectorB ; padding(1:abs(numel(vectorB)-maxLen))];
             
-            if plotMatches == 1
-                plot([vectorA(i),vectorB(i)], [1,1.1], '*-')
-            end
+            [closestA,idxA] = min(abs(vectorA(i)-vectorB));
+            [closestB,idxB] = min(abs(vectorB(i)-vectorA));
+            [~,ind] = max([idxA idxB]);
             
-            hits(1,i) = vectorA(i);
-            hits(2,i) = vectorB(i);
-            
-            vectorA(i) = inf;
-            vectorB(i) = inf;
-            
-        else % there are potentially unpaired points
-            switch idx
-                case 1 % vectorA(i) is closer to vectorB(i+X)
-                    lower = vectorB(vectorB<vectorB(idxA));
-                    if numel(lower) > 1
-                        vectorB(vectorB<lower(end)) = inf;
-                    end
-                case 2 % vectorB(i) is closer to vectorA(i+X)
-                    lower = vectorA(vectorA<vectorA(idxB));
-                    if numel(lower) > 1
-                        vectorA(vectorA<lower(end)) = inf;
-                    end
-            end
-            
-            compA = vectorA(~isinf(vectorA));
-            compB = vectorB(~isinf(vectorB));
-            maxElement = min(numel(compA),numel(compB));
-            
-            switch idx
-                case 1
-                    [~,diffAB] = min(abs(compA(1)-compB));
-                case 2
-                    [~,diffAB] = min(abs(compB(1)-compA));
-            end
-            
-            diffAB = diffAB - 1;
-            
-            % Check if future pairings (i+7) are associated with a smaller
-            % error before accepting skip and leaving point(s) unpaired
-            
-            if maxElement == 1
-                compDiff = 1;
-            elseif maxElement < 8 + diffAB
-                compA = compA(1:maxElement);
-                compB = compB(1:maxElement);
-                compDiff = maxElement-diffAB;               
-            else
-                compA = compA(1:8+diffAB);
-                compB = compB(1:8+diffAB);
-                compDiff = 8;
-            end
-            
-            switch idx
+            if idxA == idxB % vectorA(i)/vectorB(i) are each other's closest match
                 
-                case 1 % check if vectorB(i) should be unpaired
-                    
-                    if sum(abs(compA(1:compDiff)-compB(1:compDiff))) ...
-                            > sum(abs(compA(1:compDiff) ...
-                            - compB(1+diffAB:compDiff+diffAB)))
-                        
-                        asynchronies(counter) = closestA;
-                        
-                        if plotMatches == 1
-                            plot([vectorA(i),vectorB(idxA)], [1,1.1], '*-')
+                asynchronies(counter) = closestA;
+                            
+                if plotMatches == 1
+                    plot([vectorA(i),vectorB(idxA)], [1,1.1], '*-')
+                end
+                
+                hits(1,counter) = vectorA(i);
+                hits(2,counter) = vectorB(i);
+                
+                vectorA(i) = inf;
+                vectorB(i) = inf;
+                
+            else % there are potentially unpaired points
+                switch ind
+                    case 1
+                        lower = vectorB(vectorB<vectorB(idxA));
+                        if numel(lower) > 1
+                            vectorB(vectorB<lower(end)) = inf;
                         end
-                        hits(1,i) = vectorA(i);
-                        hits(2,i) = vectorB(idxA);
-                        vectorA(i) = inf;
-                        vectorB(idxA) = inf;
-                        vectorB(i:idxA-1) = [];
+                    case 2
+                        lower = vectorA(vectorA<vectorA(idxB));
+                        if numel(lower) > 1
+                            vectorA(vectorA<lower(end)) = inf;
+                            
+                        end
+                end
+                
+                compA = vectorA(~isinf(vectorA));
+                compB = vectorB(~isinf(vectorB));
+                maxElement = min(numel(compA),numel(compB));
+                
+                switch ind
+                    case 1
+                        [~,diffAB] = min(abs(compA(1)-compB));
+                    case 2
+                        [~,diffAB] = min(abs(compB(1)-compA));
+                end
+                
+                diffAB = diffAB - 1;
+                
+                if maxElement == 1
+                    compDiff = 1;
+                elseif maxElement < 8 + diffAB
+                    compA = compA(1:maxElement);
+                    compB = compB(1:maxElement);
+                    compDiff = maxElement-diffAB;
+                else
+                    compA = compA(1:8+diffAB);
+                    compB = compB(1:8+diffAB);
+                    compDiff = 8;
+                end
+                
+                switch ind
+                    
+                    case 1 % vectorB(i) may be unpaired
                         
-                    else % skipping doesn't reduce future error, only
-                        % skip if there are more than one unpaired points
-                        % before the next match
-                        
-                        if vectorB(i) ~= inf
-                            asynchronies(counter) = abs(vectorA(i)-vectorB(i));
+                        if sum(abs(compA(1:compDiff)-compB(1:compDiff))) ...
+                                > sum(abs(compA(1:compDiff) ...
+                                - compB(1+diffAB:compDiff+diffAB)))
+                            
+                            asynchronies(counter) = closestA;
                             
                             if plotMatches == 1
-                                plot([vectorA(i),vectorB(i)], [1,1.1], '*-')
+                                plot([vectorA(i),vectorB(idxA)], [1,1.1], '*-')
                             end
                             
-                            hits(1,i) = vectorA(i);
-                            hits(2,i) = vectorB(i);
+                            hits(1,counter) = vectorA(i);
+                            hits(2,counter) = vectorB(idxA);
+                            
                             vectorA(i) = inf;
-                            vectorB(i) = inf;
+                            vectorB(idxA) = inf;
+                            vectorB(i:idxA-1) = [];
+                            
                         else
-                            counter = counter - 1;
-                            vectorA = [vectorA(1:i) ; vectorA(i:end)];
-                            vectorA(i) = inf;
+                            
+                            if vectorB(i) ~= inf
+                                
+                                asynchronies(counter) = abs(vectorA(i)-vectorB(i));
+
+                                if plotMatches == 1
+                                    plot([vectorA(i),vectorB(i)], [1,1.1], '*-')
+                                end
+                                
+                                hits(1,counter) = vectorA(i);
+                                hits(2,counter) = vectorB(i);
+                                
+                                vectorA(i) = inf;
+                                vectorB(i) = inf;
+                                
+                            else
+                                % vectorB(i) is inf now!
+                                counter = counter - 1;
+                                vectorA = [vectorA(1:i) ; vectorA(i:end)];
+                                vectorA(i) = inf;
+                            end
+                            
                         end
                         
-                    end
-                    
-                case 2 % check if vectorA(i) should be unpaired
-                    
-                    if sum(abs(compB(1:compDiff)-compA(1:compDiff))) ...
-                            > sum(abs(compB(1:compDiff) ...
-                            - compA(1+diffAB:compDiff+diffAB)))
+                    case 2 % vectorA(i) may be unpaired
                         
-                        asynchronies(counter) = closestB;
-                        
-                        if plotMatches == 1
-                            plot([vectorA(idxB),vectorB(i)], [1,1.1], '*-')
-                        end
-                        hits(1,i) = vectorA(idxB);
-                        hits(2,i) = vectorB(i);
-                        vectorB(i) = inf;
-                        vectorA(idxB) = inf;
-                        vectorA(i:idxB-1) = [];
-                        
-                    else % skipping doesn't reduce future error, only
-                        % skip if there are more than one unpaired points
-                        % before the next match
-                        
-                        if vectorA(i) ~= inf
-                            asynchronies(counter) = abs(vectorB(i)-vectorA(i));
+                        if sum(abs(compB(1:compDiff)-compA(1:compDiff))) ...
+                                > sum(abs(compB(1:compDiff) ...
+                                - compA(1+diffAB:compDiff+diffAB)))
+                            
+                            asynchronies(counter) = closestB;
                             
                             if plotMatches == 1
-                                plot([vectorA(i),vectorB(i)], [1,1.1], '*-')
-                            end                             
-                            hits(1,i) = vectorA(i);
-                            hits(2,i) = vectorB(i);
-                            vectorA(i) = inf;
+                                plot([vectorA(idxB),vectorB(i)], [1,1.1], '*-')
+                            end
+                            
+                            hits(1,counter) = vectorA(idxB);
+                            hits(2,counter) = vectorB(i);
+                            
                             vectorB(i) = inf;
+                            vectorA(idxB) = inf;
+                            vectorA(i:idxB-1) = [];
+                            
                         else
-                            counter = counter - 1;
-                            vectorB = [vectorB(1:i) ; vectorB(i:end)];
-                            vectorB(i) = inf;     
+                            
+                            if vectorA(i) ~= inf
+                                asynchronies(counter) = abs(vectorB(i)-vectorA(i));
+                                
+                                if plotMatches == 1
+                                    plot([vectorA(i),vectorB(i)], [1,1.1], '*-')
+                                end
+                                
+                                hits(1,counter) = vectorA(i);
+                                hits(2,counter) = vectorB(i);
+                                
+                                vectorA(i) = inf;
+                                vectorB(i) = inf;
+                                
+                            else
+                                counter = counter - 1;
+                                vectorB = [vectorB(1:i) ; vectorB(i:end)];
+                                vectorB(i) = inf;
+                                
+                            end
+                            
                         end
                         
-                    end
-                    
+                end
             end
+            
         end
+        counter = counter + 1;
+    end
+    
+    if longerVector == 1
+        
+        matched1 = find(ismember(vector1,hits(2,:)));
+        matched2 = find(ismember(vector2,hits(1,:)));
+        unmatched1 = find(~ismember(vector1,hits(2,:)));
+        unmatched2 = find(~ismember(vector2,hits(1,:)));
+        
+    else
+        
+        matched1 = find(ismember(vector1,hits(1,:)));
+        matched2 = find(ismember(vector2,hits(2,:)));
+        unmatched1 = find(~ismember(vector1,hits(1,:)));
+        unmatched2 = find(~ismember(vector2,hits(2,:)));
         
     end
-    counter = counter + 1;
+    
+else
+    asynchronies = NaN;
+    unmatched1 = NaN;
+    unmatched2 = NaN;
+    matched1 = NaN; 
+    matched2 = NaN;
 end
 
+asynchronies = asynchronies';
 
-if longerVector == 1 
-    [~,unmatched2] = setdiff(vector2,hits(1,:));    
-    [~,unmatched1] = setdiff(vector1,hits(2,:));
-else       
-    [~,unmatched2] = setdiff(vector2,hits(2,:));    
-    [~,unmatched1] = setdiff(vector1,hits(1,:));
+if plotMatches == 1
+    xlabel('Observations');
+    xticks(1:10:max([vector1 ; vector2]));
+    xticklabels([]);
+    yticks([1 1.1]);
+    if longerVector == 1
+        yticklabels([2 1]);
+    else
+        yticklabels([1 2]);
+    end
+    grid on
+    set(gcf, 'Position', [20, 200, 800, 350])
+    title('Mutual Closest Matches')
+    set(gcf,'color','w')
 end
 
-
-set(gca,'YTick', [])
 
 end
